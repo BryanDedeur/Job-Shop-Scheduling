4b519b294ae78b1423af1643f120cc9a31da5d0c
BryanDedeur <bdedeurwaerder@nevada.unr.edu>
2019-07-12 14:25:12 -0700

    Update computing non optimized schedule

--- Assets/Scripts/JobShopSchedulingScript.cs
+++ Assets/Scripts/JobShopSchedulingScript.cs
@@ -18,13 +18,16 @@ public class JobShopSchedulingScript : MonoBehaviour
     private int NumJobs;
     private int NumMachines;
 
-    private List<List<int>> JobSequence;
+
+    // first list is jobs, seconds list is machines
+    private List<List<Vector3>> JobSchedule;
+
 
     private void ParseMatrix()
     {
         char[] separators = new char[] { ' ' };
 
-        JobSequence = new List<List<int>>();
+        JobSchedule = new List<List<Vector3>>();
 
         if (JobMatrix == "")
         {
@@ -62,27 +65,80 @@ public class JobShopSchedulingScript : MonoBehaviour
                     }
                 } else
                 {
-                    List<int> temp = new List<int>();
+                    List<Vector3> jobSequence = new List<Vector3>();
+                    Vector3 jobSequenceInfo = new Vector3();
 
+                    bool isMachineID = true;
                     foreach (var value in textLines[i].Split(separators, StringSplitOptions.RemoveEmptyEntries))
                     {
-                        int number;
 
+                        int number = 0;
                         bool success = Int32.TryParse(value, out number);
                         if (success)
                         {
-                            Console.WriteLine("Converted '{0}' to {1}.", value, number);
-                            temp.Add(number);
+                            if (isMachineID == true)
+                            {
+                                jobSequenceInfo.x = number;
+                            }
+                            else
+                            {
+                                jobSequenceInfo.y = number;
+                                jobSequence.Add(jobSequenceInfo);
+                                jobSequenceInfo = new Vector3();
+                            }
+                            isMachineID = !isMachineID;
+                            //Console.WriteLine("Converted '{0}' to {1}.", value, number);
+
                         }
                         else
                         {
-                            Console.WriteLine("Attempted conversion of '{0}' failed.",
+                            Console.WriteLine("AtjobSequenceted conversion of '{0}' failed.",
                                                value ?? "<null>");
                             break;
                         }
                     }
-                    JobSequence.Add(temp);
+                    JobSchedule.Add(jobSequence);
                 }
+            }
+        }
+    }
+
+
+    // jobNumber is the index of the job to prioritize using 0 base indexing
+    void PrioritizeJob(int jobNumber)
+    {
+        // make sure job number is valid
+        if (jobNumber < NumJobs && jobNumber > -1)
+        {
+            float lastStartTime = 0;
+            // compute the start times for jobNumber as starting first
+            for (int task = 0; task < NumMachines; task++)
+            {
+                Vector3 taskInfo = JobSchedule[jobNumber][task];
+                taskInfo.z = taskInfo.z + lastStartTime;
+                JobSchedule[jobNumber][task] = taskInfo;
+                lastStartTime = lastStartTime + JobSchedule[jobNumber][task].y;
+            }
+
+            lastStartTime = 0;
+            for (int job = 0; job < jobNumber; job++)
+            {
+                for (int task = 0; task < NumMachines; task++)
+                {
+                    int machineID = task;
+                    
+                }
+            }
+        }
+
+
+
+        // adjust the schedules for all the other tasks
+        for (int job = jobNumber + 1; job < NumJobs; job++)
+        {
+            for (int task = 0; task < NumMachines; task++)
+            {
+                
             }
         }
     }
@@ -91,13 +147,14 @@ public class JobShopSchedulingScript : MonoBehaviour
     void Start()
     {
         ParseMatrix();
+        PrioritizeJob(0);
         print(MatrixDescription);
         for (int x = 0; x < NumJobs; x++)
         {
             string printStuff = "";
-            for (int y = 0; y < NumMachines * 2; y++)
+            for (int y = 0; y < NumMachines; y++)
             {
-                printStuff = printStuff + JobSequence[x][y] + " ";
+                printStuff = printStuff + JobSchedule[x][y] + " ";
             }
             print(printStuff);
         }
